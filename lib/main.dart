9ebc0ef38a7ae5be3c9c5be053e192b3746117db
yintii <keleheart1@gmail.com>
2025-06-16 13:16:57 -0700

    Added nav drawer with a logout option so I can log in and out and test users without hot restarting

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'scanner_page.dart';
 import 'todayspatrons_page.dart';
 import 'pending_incidents_page.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:id_tracker_app/services/current_user.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: ".env");
@@ -32,10 +33,46 @@ class MyApp extends StatelessWidget {
 
 // Optional: still include MainPage as a placeholder for now
 class MainPage extends StatelessWidget {
+
+  var currentUserRole = CurrentUser().role;
+
   @override
   Widget build(BuildContext context) {
+
+    void _handleLogout(){
+      CurrentUser().clear();
+      Navigator.pushNamedAndRemoveUntil(context, '/login', (route) => false);
+    }
+
     return Scaffold(
       appBar: AppBar(title: Text('Main Page')),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(color: Colors.blue),
+              child: Text('ID Tracker Menu', style: TextStyle(color: Colors.white, fontSize: 24)),
+            ),
+            if(currentUserRole == 'Manager')
+              ListTile(
+                leading: Icon(Icons.report),
+                title: Text('Pending Incidents'),
+                onTap: (){
+                  Navigator.pop(context);
+                  Navigator.pushNamed(context, '/incidents/pending');
+                },
+              ),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text('Logout'),
+              onTap: (){
+                _handleLogout();
+              }
+            ),
+          ],
+        ),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -47,13 +84,15 @@ class MainPage extends StatelessWidget {
               child: Text('Scan License'),
             ),
             SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/incidents/pending'); // Add this when incident list is ready
-              },
-              child: Text('View Incidents'),
-            ),
-            SizedBox(height: 20),
+            if (currentUserRole == "manager")
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/incidents/pending'); // Add this when incident list is ready
+                },
+                child: Text('View Incidents'),
+              ),
+            if(currentUserRole == "manager")
+              SizedBox(height: 20),
             ElevatedButton(
               onPressed: (){
                 Navigator.pushNamed(context, '/patrons');
